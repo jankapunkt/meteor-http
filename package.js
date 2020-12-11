00aeb43b754abcb08dfb0f906f828422f00b3200
Jan Küster <jkuester@uni-bremen.de>
2020-12-11 12:38:01 +0100

    published 2.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jkuester:http',
-  version: '2.0.0',
+  version: '2.0.1',
   // Brief, one-line summary of the package.
   summary: 'Make HTTP calls to remote servers using fetch API',
   // URL to the Git repository containing the source code for this package.
